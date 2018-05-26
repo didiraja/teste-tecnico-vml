8e2b04d12946ea00d7cbcd0850604d8f178b8de1
Dico Didiraja <didiraja@hotmail.com>
2018-05-26 15:24:25 -0300

    incluido gulp e sass no projeto

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp        = require('gulp'),
+	browserSync = require('browser-sync').create(),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	
+	return gulp.src('src/css/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+    	.pipe(gulp.dest('src/css'));
+	
+});
+
+gulp.task('reload', function () {
+	
+	
+	
+});
+
+gulp.task('server', function() {
+	
+	browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+	
+	gulp.watch("src/css/**/*", ['sass']);
+	gulp.watch("src/**/*").on('change', browserSync.reload);
+	
+});
